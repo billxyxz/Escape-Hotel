3a4b94c644c85f5283522323b36831b1aa06f506
Collins Erobaga <erobagacollins@gmail.com>
2023-06-09 15:21:08 +0100

    First official commit

---
+++ script.js
@@ -0,0 +1,152 @@
+const MENU = () => {
+    const navbar = document.getElementById("navbar")
+    const hamburger = document.querySelector(".hamburger");
+    const menu = document.querySelector(".nav-links");
+    const navLinks = document.querySelectorAll(".nav-links li");
+
+    window.addEventListener("scroll", () => {
+        if(window.scrollY > navbar.offsetHeight + 70){
+            navbar.style.backgroundColor = "#19346D"
+        }else{
+            navbar.style.backgroundColor = "transparent"
+        }
+    })
+
+    hamburger.addEventListener("click", () => {
+        menu.classList.toggle("active");
+
+        navLinks.forEach((link, idx) => {
+            if(link.style.animation){
+                link.style.animation = "";
+            }else{
+                link.style.animation = `navFade 0.5s ease forwards ${idx/6 + 1}s`
+            }
+
+        });
+
+        hamburger.classList.toggle("toggle")
+
+    })
+    
+        
+};
+const the_animation = document.querySelectorAll(".animation");
+const contacts_animation = document.querySelectorAll(".box")
+const  checker = new IntersectionObserver((entries) => {
+    entries.forEach((entry) => {
+        if(entry.isIntersecting){
+            entry.target.classList.add("scroll-animation");
+        }else{
+            entry.target.classList.remove("scroll-animation");
+        }
+    });
+
+})
+
+console.log(contacts_animation)
+
+console.log(the_animation)
+
+for(let i = 0; i < the_animation.length; i++){
+    const el = the_animation[i];
+
+    checker.observe(el);
+
+}
+
+const  observer = new IntersectionObserver((entries) => {
+    entries.forEach((entry) => {
+        if(entry.isIntersecting){
+            entry.target.classList.add("animate");
+        }else{
+            entry.target.classList.remove("animate");
+        }
+    });
+
+})
+
+for(let i = 0; i < contacts_animation.length; i++){
+    const el = contacts_animation[i];
+
+    observer.observe(el);
+
+}
+
+const MODAL = () => {
+    const startBtn = document.querySelector(".modal-btn");
+    const modal = document.querySelector(".modal-cont");
+    const remove = document.querySelector(".remove")
+
+    startBtn.addEventListener("click", () => {
+        modal.classList.add("active");
+        remove.addEventListener("click", () => {
+            modal.classList.remove("active")
+        })
+
+    })
+
+
+}
+
+const DESC = () => {
+    const openBtn = document.querySelector(".read-btn")
+    const desc = document.querySelector(".desc")
+
+    openBtn.addEventListener("click", () => {
+        desc.classList.toggle("open");
+    })
+}
+
+
+
+const galleryModal = () => {
+    const gallery = document.querySelector(".gallery-modal");
+    const view = document.querySelector(".gallery-img");
+    const images = document.querySelectorAll(".view");
+    const close = document.querySelector(".close");
+    const leftBtn = document.querySelector(".left");
+    const rightBtn = document.querySelector(".right")
+
+    let imagesArr = ["http://127.0.0.1:5500/images/lobby-large.jpg", "http://127.0.0.1:5500/images/people-dinning-large.jpg", "http://127.0.0.1:5500/images/room-1-large.jpg", "http://127.0.0.1:5500/images/room-2-large.jpg", "http://127.0.0.1:5500/images/pool-large.jpg", "http://127.0.0.1:5500/images/room-3-large.jpg", "http://127.0.0.1:5500/images/hotel-bar-large.jpg", "http://127.0.0.1:5500/images/room-4-large.jpg"]
+
+   let i;
+
+
+    images.forEach((image) => {
+        image.addEventListener("click", () => {
+
+            gallery.classList.add("active")
+
+            const link = image.src
+            source = link.replace("small", "large");
+            view.src = source;
+            if(imagesArr.includes(view.src)){
+                i = imagesArr.indexOf(view.src);
+            }
+            leftBtn.addEventListener("click", () => {
+                i--;
+                if(i < 0){
+                    i = imagesArr.length - 1
+                }
+                view.src = imagesArr[i]
+            });
+            rightBtn.addEventListener("click", () => {
+                i++;
+                if(i > imagesArr.length - 1){
+                    i = 0
+                }
+                view.src = imagesArr[i]
+            })
+        })
+
+        close.addEventListener("click", () => {
+            gallery.classList.remove("active");
+            
+        })
+    })
+} 
+
+MENU();
+MODAL();
+DESC();
+galleryModal();
